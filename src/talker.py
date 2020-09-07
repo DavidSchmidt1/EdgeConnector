6bad5087993bbe891f5e36e3d863c53c318398d1
DavidSchmidt1 <david.schmidt1@live.de>
2020-09-07 14:04:31 +0200

    self.input1 added

--- src/talker.py
+++ src/talker.py
@@ -9,7 +9,7 @@ import rospy
 
 
 class Connector(object):
-    def __init__():
+    def __init__(self):
         print("Starting ROSConnector Module")
         print("Connect Client...")
         self.module_client = IoTHubModuleClient.create_from_edge_environment()
@@ -19,7 +19,7 @@ class Connector(object):
         # connect the client.
         print("Client Connected!")
         self.r = rospy.Rate(10) # 10hz
-    async def input1_listener(module_client):
+    async def input1_listener(self,module_client):
         print("starting input listener..")
         while True:
             input_message = await module_client.receive_message_on_input("input1")  # blocking call
@@ -28,9 +28,9 @@ class Connector(object):
             pub.publish(input_message.data)
             print("custom properties are")
             print(input_message.custom_properties)
-    async def main():
+    async def main(self):
         await self.module_client.connect()
-        listeners = asyncio.gather(input1_listener(self,module_client))
+        listeners = asyncio.gather(self.input1_listener(self,module_client))
         self.r.sleep()
 
 
