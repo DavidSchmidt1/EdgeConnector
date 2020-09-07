daa01d7401e26271875ad47d1c3edce42a6ba4aa
DavidSchmidt1 <david.schmidt1@live.de>
2020-09-07 09:23:20 +0200

    added logs and error handling

--- src/talker.py
+++ src/talker.py
@@ -10,52 +10,44 @@ import rospy
 
 async def main():
     # The client object is used to interact with your Azure IoT hub.
-    module_client = IoTHubModuleClient.create_from_edge_environment()
+    try: 
+        print("Starting ROSConnector Module")
+        print("Connect Client...")
+        module_client = IoTHubModuleClient.create_from_edge_environment()
+   
+        print("....")
+        # connect the client.
+        await module_client.connect()
+    
+        print("Client Connected!")
+        pub = rospy.Publisher('blocked', String, queue_size=2)
+        rospy.init_node('detection')
+        r = rospy.Rate(10) # 10hz
+        # define behavior for receiving an input message on input1
+        async def input1_listener(module_client):
+            print("starting input listener..")
+            while True:
+                input_message = await module_client.receive_message_on_input("input1")  # blocking call
+                print("the data in the message received on input1 was ")
+                print(input_message.data)
+                pub.publish(input_message.data)
+                print("custom properties are")
+                print(input_message.custom_properties)
+          
 
-    # connect the client.
-    await module_client.connect()
-    pub = rospy.Publisher('blocked', String, queue_size=2)
-    rospy.init_node('detection')
-    r = rospy.Rate(10) # 10hz
-    # define behavior for receiving an input message on input1
-    async def input1_listener(module_client):
-        while True:
-            input_message = await module_client.receive_message_on_input("input1")  # blocking call
-            print("the data in the message received on input1 was ")
-            print(input_message.data)
-            pub.publish(input_message.data)
-            print("custom properties are")
-            print(input_message.custom_properties)
-
-    # define behavior for receiving an input message on input2
-    async def input2_listener(module_client):
-        while True:
-            input_message = await module_client.receive_message_on_input("input2")  # blocking call
-            print("the data in the message received on input2 was ")
-            print(input_message.data)
-            print("custom properties are")
-            print(input_message.custom_properties)
-
-    # define behavior for halting the application
-    def stdin_listener():
-        while True:
-            selection = input("Press Q to quit\n")
-            if selection == "Q" or selection == "q":
-                print("Quitting...")
-                break
-
-    # Schedule task for listeners
-    listeners = asyncio.gather(input1_listener(module_client), input2_listener(module_client))
+    
+        # Schedule task for listeners
+        listeners = asyncio.gather(input1_listener(module_client))
+        r.sleep()
+        # Cancel listening
+    except Exception as e: 
+        listeners.cancel()
+        # Finally, disconnect
+        await module_client.disconnect()
+        print("shuting down everything due to:"+ str(e))
 
     
 
-    
-    r.sleep()
-    # Cancel listening
-    listeners.cancel()
-
-    # Finally, disconnect
-    await module_client.disconnect()
 
 
 if __name__ == "__main__":
@@ -66,5 +58,8 @@ if __name__ == "__main__":
         while True:
             
             loop.run_until_complete(main())
-    except: 
+            
+    except Exception as e: 
+        print("Loop had to be closed due to:"+ str(e))
         loop.close()
+        
